2c13fa27db8d9a0f681eca2b8fbcfabcfe268fa4
Simon <simon.ritzel+git@gmail.com>
2017-12-02 11:00:46 +0100

    Flashing lights script for day2

---
+++ day2/blinkenlights2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from gpiozero import LED
+from time import sleep
+
+red = LED(17)
+green = LED(18)
+
+green.on()
+red.off()
+
+while True:
+	green.on()
+    red.off()
+	sleep(0.5)
+	green.off()
+    red.on()
+	sleep(0.5)
