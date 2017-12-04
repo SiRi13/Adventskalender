61c9baa995220bda37d514075b70d78a4bc32e2c
Simon <simon.ritzel@gmail.com>
2017-12-04 08:17:45 +0100

    Surrounded while-loop with try-except and added time.sleep

--- day4/buttonLights.py
+++ day4/buttonLights.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import RPi.GPIO as GPIO
+import time
 
 GPIO.set_mode(GPIO.BCM)
 
@@ -8,14 +9,16 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(24, GPIO.IN, GPIO.PUD_DOWN)
 
-while True:
-    GPIO.output(17, 1)
-    GPIO.output(18, 0)
-    while not GPIO.input(24):
-        # do nothing
-    GPIO.output(17, 0)
-    GPIO.output(18, 1)
-    while not GPIO.input(24):
-        # do nothing
+try:
+    while True:
+        GPIO.output(17, 1)
+        GPIO.output(18, 0)
+        while not GPIO.input(24):
+            time.sleep(0.2)
+        GPIO.output(17, 0)
+        GPIO.output(18, 1)
+        while not GPIO.input(24):
+            time.sleep(0.2)
 
-GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
