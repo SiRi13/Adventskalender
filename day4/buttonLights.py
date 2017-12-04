c0d4dfcd5d50a7896d15c7dc14a84847f956fa5b
Simon <simon.ritzel@gmail.com>
2017-12-04 08:26:54 +0100

    Pulled sleep out of while-loop

--- day4/buttonLights.py
+++ day4/buttonLights.py
@@ -14,11 +14,13 @@ try:
         GPIO.output(17, 1)
         GPIO.output(18, 0)
         while not GPIO.input(24):
-            time.sleep(0.2)
+            pass
+        time.sleep(0.2)
         GPIO.output(17, 0)
         GPIO.output(18, 1)
         while not GPIO.input(24):
-            time.sleep(0.2)
+            pass
+        time.sleep(0.2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
