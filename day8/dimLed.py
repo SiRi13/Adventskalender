f94a31ea151fe69592639de13b588714c6f9cf9b
Simon <simon.ritzel@gmail.com>
2017-12-08 09:43:39 +0100

    Fixed fast in/decreasing; Print new value;

--- day8/dimLed.py
+++ day8/dimLed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,18 +13,22 @@ GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(UP, GPIO.IN, GPIO.PUD_DOWN)
 GPIO.setup(DOWN, GPIO.IN, GPIO.PUD_DOWN)
 
-pwm = GPIO.PWM(LED, 50) # init pwm with 100Hz
+pwm = GPIO.PWM(LED, 500)
 pwm_dc = 0 
 pwm.start(pwm_dc)
 
 try:
     while True:
         if GPIO.input(UP) == True and pwm_dc < 100:
-            pwm_dc += 10
+            pwm_dc += 5 
+            print("Increase pwm_dc by 5 to ", pwm_dc)
         
         if GPIO.input(DOWN) == True and pwm_dc > 0:
-            pwm_dc -= 10
+            pwm_dc -= 5 
+            print("Decrease pwm_dc by 5 to ", pwm_dc)
 
+        # avoid to fast in/decreasing of pwm_dc
+        time.sleep(0.5)
         pwm.ChangeDutyCycle(pwm_dc)
 
 except KeyboardInterrupt:
