0729c34e9133798780e7373e98d078c7b767e06c
Simon <simon.ritzel@gmail.com>
2017-12-11 11:06:45 +0100

    Day 9 README, script, image, and gif

---
+++ day9/README.md
@@ -0,0 +1,21 @@
+## Day 9
+
+### Contents
+![Contents of Day 9](assets/IMG_20171210_110500.jpg)
+
+Amount | Name | Note
+--- | --- | ---
+1 | Pin Header | 3x1
+
+### Task
+The same task as on day 8 but implemented in Python.  
+See [Day 8](../day8/README.md#Task)
+
+![Circuit of Day 9](assets/IMG_20171211_102557.jpg)
+Circuit of Day 9
+
+### Result
+![Result of Day 9](assets/day9_dimBrightenLed.gif)
+Result of Day 9
+
+Python script: [dimLed.py](dimLed.py)
---
+++ day9/dimLed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+LED = 4
+UP = 8
+DOWN = 7
+
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(UP, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(DOWN, GPIO.IN, GPIO.PUD_DOWN)
+
+pwm = GPIO.PWM(LED, 500)
+pwm_dc = 0 
+pwm.start(pwm_dc)
+
+try:
+    while True:
+        if GPIO.input(UP) == True and pwm_dc < 100:
+            pwm_dc += 5 
+            print("Increase pwm_dc by 5 to ", pwm_dc)
+        
+        if GPIO.input(DOWN) == True and pwm_dc > 0:
+            pwm_dc -= 5 
+            print("Decrease pwm_dc by 5 to ", pwm_dc)
+
+        # avoid fast in/decreasing of pwm_dc
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(pwm_dc)
+
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
