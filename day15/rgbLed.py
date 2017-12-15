cd70f2928ca4ff8dd1e4a9e1f576e06aefcd3929
Simon <simon.ritzel@gmail.com>
2017-12-15 07:35:15 +0100

    Add python script

---
+++ day15/rgbLed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GREEN   = 5
+BLUE    = 6
+RED     = 7
+
+GPIO.setup(GREEN, GPIO.OUT) 
+GPIO.setup(BLUE, GPIO.OUT) 
+GPIO.setup(RED, GPIO.OUT) 
+
+GPIO.output(GREEN, 1)
+GPIO.output(BLUE, 0)
+GPIO.output(RED, 0)
+
+try:
+    while True:
+        GPIO.output(RED, 1)
+        time.sleep(0.1)
+        GPIO.output(GREEN, 0)
+        time.sleep(0.1)
+        GPIO.output(BLUE, 1)
+        time.sleep(0.1)
+        GPIO.output(RED, 0)
+        time.sleep(0.1)
+        GPIO.output(GREEN, 1)
+        time.sleep(0.1)
+        GPIO.output(BLUE, 0)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
