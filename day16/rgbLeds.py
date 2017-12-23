57d2eebed5202085ec82a8e551f12b847b8c27fe
Simon <simon.ritzel@gmail.com>
2017-12-23 18:42:13 +0100

    Remove file and modified algorithm

--- day16/rgbLeds.py
+++ day16/rgbLeds.py
@@ -7,112 +7,132 @@ import time
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
-RED1, GREEN1, BLUE1 = 5, 6, 7
-RED2, GREEN2, BLUE2 = 8, 9, 10
+RED1, GREEN1, BLUE1 = 7, 5, 6
+RED2, GREEN2, BLUE2 = 10, 7, 8
 
-r1, g1, b1 = 0.0, 0.0, 0.0
-r1t, g1t, b1t = 0.0, 0.0, 0.0
+r1, g1, b1 = 0, 0, 0
+r1t, g1t, b1t = 0, 0, 0
 
-r2, g2, b2 = 0.0, 0.0, 0.0
-r2t, g2t, b2t = 0.0, 0.0, 0.0
+r2, g2, b2 = 0, 0, 0
+r2t, g2t, b2t = 0, 0, 0
 
-@app.route('/_day16')
+@app.route('/day16/red1', methods=['POST'])
+def red1():
+    global r1, r1t
+
+    r1 = request.form.get('red1', 0, type=int)
+    r1t = request.form.get('red1time', 0, type=int)
+
+    return jsonify(result='red1 set')
+
+@app.route('/day16/red2', methods=['POST'])
+def red2():
+    global r2, r2t
+
+    r2 = request.form.get('red2', 0, type=int)
+    r2t = request.form.get('red2time', 0, type=int)
+
+    return jsonify(result="red2 set")
+
+@app.route('/day16/green1', methods=['POST'])
+    global g1, g1t
+
+    g1 = request.form.get('green1', 0, type=int)
+    g1t = request.form.get('green1time', 0, type=int)
+
+    return jsonify(result="green1 set")
+
+@app.route('/day16/green2', methods=['POST'])
+    global g2, g2t
+
+    g2 = request.form.get('green2', 0, type=int)
+    g2t = request.form.get('green2time', 0, type=int)
+    
+    return jsonify(result="green2 set")
+
+@app.route('/day16/blue1', methods=['POST'])
+    global b1, b1t
+
+    b1 = request.form.get('blue1', 0, type=int)
+    b1t = request.form.get('blue1time', 0, type=int)
+
+    return jsonify(result="blue1 set")
+
+@app.route('/day16/blue2', methods=['POST'])
 def day16():
-    global r1, r2, g1, g2, b1, b2
-    global r1t, g1t, b1t
-    global r2t, g2t, b2t
+    global b2, b2t
 
-    r1 = request.args.get('red1', 0, type=float)
-    g1 = request.args.get('green1', 0, type=float)
-    b1 = request.args.get('blue1', 0, type=float)
-    r1t = request.args.get('red1time', 0, type=float)
-    g1t = request.args.get('green1time', 0, type=float)
-    b1t = request.args.get('blue1time', 0, type=float)
+    b2 = request.form.get('blue2', 0, type=int)
+    b2t = request.form.get('blue2time', 0, type=int)
 
-    r2 = request.args.get('red2', 0, type=float)
-    g2 = request.args.get('green2', 0, type=float)
-    b2 = request.args.get('blue2', 0, type=float)
-    r2t = request.args.get('red2time', 0, type=float)
-    g2t = request.args.get('green2time', 0, type=float)
-    b2t = request.args.get('blue2time', 0, type=float)
+    return jsonify(result="blue2 set")
 
-    return jsonify(result=True)
-
-@app.route("/")
+@app.route("/day16")
 def index():
+    GPIO.setmode(GPIO.BCM)
+    threads = []
+    for i in [RED1, GREEN1, BLUE1, RED2, GREEN2, BLUE2]:
+        t = threading.Thread(target=gpio_thread, args=(i,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
     return render_template('index.html')
 
 def gpio_thread(ledId):
-    global r1, r2, g1, g2, b1, b2
-    global r1t, r2t, g1t, g2t, b1t, b2t
-
+    GPIO.setup(ledId, GPIO.OUT)
+    pwm = GPIO.PWM(ledId, 50)
+    
     if ledId == RED1:
-        GPIO.setup(RED1, GPIO.OUT)
-        pwm_r1 = GPIO.PWM(RED1, 50)
-        pwm_r1.start(r1)
+        pwm.start(r1)
         while True:
-            if 0 < r1 < 100:
-                pwm_r1.ChangeDutyCycle(r1)
+            if 0 <= r1 <= 100:
+                pwm.ChangeDutyCycle(r1)
             time.sleep(r1t)
+        pwm.stop()
 
-        pwm_r1.stop()
     elif ledId == GREEN1:
-        GPIO.setup(GREEN1, GPIO.OUT)
-        pwm_g1 = GPIO.PWM(GREEN1, 50)
-        pwm_g1.start(g1)
+        pwm.start(g1)
         while True:
             if 0 < g1 < 100:
-                pwm_g1.ChangeDutyCycle(g1)
+                pwm.ChangeDutyCycle(g1)
             time.sleep(g1t)
-        pwm_g1.stop()
+        pwm.stop()
+        
     elif ledId == BLUE1:
-        GPIO.setup(BLUE1, GPIO.OUT)
-        pwm_b1 = GPIO.PWM(BLUE1, 50)
-        pwm_b1.start(b1)
+        pwm.start(b1)
         while True:
             if 0 < b1 < 100:
-                pwm_b1.ChangeDutyCycle(b1)
+                pwm.ChangeDutyCycle(b1)
             time.sleep(b1t)
-        pwm_b1.stop()
+        pwm.stop()
+        
     elif ledId == RED2:
-        GPIO.setup(RED2, GPIO.OUT)
-        pwm_r2 = GPIO.PWM(RED2, 50)
-        pwm_r2.start(r2)
+        pwm.start(r2)
         while True:
             if 0 < r2 < 100:
-                pwm_r2.ChangeDutyCycle(r2)
+                pwm.ChangeDutyCycle(r2)
             time.sleep(r2t)
-        pwm_r2.stop()
+        pwm.stop()
+        
     elif ledId == GREEN2:
-        GPIO.setup(GREEN2, GPIO.OUT)
-        pwm_g2 = GPIO.PWM(GREEN2, 50)
-        pwm_g2.start(g2)
+        pwm.start(g2)
         while True:
             if 0 < g2 < 100:
-                pwm_g2.ChangeDutyCycle(g2)
+                pwm.ChangeDutyCycle(g2)
             time.sleep(g2t)
-        pwm_g2.stop()
+        pwm.stop()
+        
     elif ledId == BLUE2:
-        GPIO.setup(BLUE2, GPIO.OUT)
-        pwm_b2 = GPIO.PWM(BLUE2, 50)
-        pwm_b2.start(b2)
+        pwm.start(b2)
         while True:
             if 0 < b2 < 100:
-                pwm_b2.ChangeDutyCycle(b2)
+                pwm.ChangeDutyCycle(b2)
             time.sleep(b2t)
-        pwm_b2.stop()
+        pwm.stop()
 
 
 if __name__ == '__main__':
     app.run()
-
-    GPIO.setmode(GPIO.BCM)
-    threads = []
-    for i in [RED1, GREEN1, BLUE1]:
-        t = threading.Thread(target=gpio_thread, args=(i,))
-        threads.append(t)
-        t.start()
-        threads.append(t)
-
-    for t in threads:
-        t.join()
