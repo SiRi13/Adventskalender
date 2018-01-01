1b38cf33aa25306f25b953bf538f4e86458ef70b
Simon <simon.ritzel@gmail.com>
2018-01-01 18:24:11 +0100

    Add mp3s and corrected path to files

--- day24/adventCountdown.py
+++ day24/adventCountdown.py
@@ -20,15 +20,15 @@ time.sleep(0.5)
 pwm.ChangeDutyCycle(0)
 
 if m > 4:
-    subprocess.Popen(["omxplayer", "lied1.mp3"])
+    subprocess.Popen(["omxplayer", "assets/lied1.mp3"])
 elif m > 7:
-    subprocess.Popen(["omxplayer", "lied2.mp3"])
+    subprocess.Popen(["omxplayer", "assets/lied2.mp3"])
 elif m > 10:
-    subprocess.Popen(["omxplayer", "lied3.mp3"])
+    subprocess.Popen(["omxplayer", "assets/lied3.mp3"])
 elif m > 12:
-    subprocess.Popen(["omxplayer", "lied4.mp3"])
+    subprocess.Popen(["omxplayer", "assets/lied4.mp3"])
 else:
-    subprocess.Popen(["omxplayer", "lied5.mp3"])
+    subprocess.Popen(["omxplayer", "assets/lied5.mp3"])
 
 pwm.stop()
 GPIO.cleanup()
